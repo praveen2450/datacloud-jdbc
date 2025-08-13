a9ebeb2ac537674645070ab045a929c949a43437
Praveen Kumar <praveenkumar2450@gmail.com>
2025-08-13 14:29:35 +0530

    Fix build issue caused by merge conflict

--- spark-datasource/src/main/scala/com/salesforce/datacloud/spark/HyperResultSource.scala
+++ spark-datasource/src/main/scala/com/salesforce/datacloud/spark/HyperResultSource.scala
@@ -5,6 +5,7 @@ import org.apache.spark.sql.types.StructType
 import org.apache.spark.sql.util.CaseInsensitiveStringMap
 import org.apache.spark.sql.connector.catalog.Table
 import org.apache.spark.sql.connector.expressions.Transform
+import scala.util.Using
 import com.salesforce.datacloud.spark.core.{
   HyperConnectionOptions,
   HyperResultTable,
